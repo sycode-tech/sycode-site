48983124006b16b2f1c78fbfcae346c705985e8b
Lucas Glaydson <sycode.devv@gmail.com>
2023-10-06 21:32:05 -0300

    manutenção

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,5 +1,5 @@
-import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 import Image from "next/image";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 
 export const SocialMedia = () => {
   return (
