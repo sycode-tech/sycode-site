69687924fd145288a8ecfe4f0b22293894f50853
Lucas Glaydson <sycode.devv@gmail.com>
2023-10-06 21:06:14 -0300

    manutenção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="w-full h-screen bg-gradient grid place-content-center">
       <link rel="icon" href="..\icons\logo.ico" type="image/x-icon" />
       <Image alt="Logo-Sycode" src={"/icons/logo-light.svg"} width={210} height={270} className="Image"></Image>
+      <h2>Em Manutenção</h2>
     </main>
     </>
   );
